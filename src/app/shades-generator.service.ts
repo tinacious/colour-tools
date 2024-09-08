4ad3b01ee24291e366f2e8deaa019c34be316a03
@tinacious <info@tinaciousdesign.com>
2024-09-08 15:33:39 -0400

    form for generating colour swatches

--- src/app/shades-generator.service.ts
+++ src/app/shades-generator.service.ts
@@ -8,7 +8,7 @@ export class ShadesGeneratorService {
 
   constructor() { }
 
-  shadesFromColour(colour: string, count: number): ColourResult[] {
+  shadesFromColour(colour: string, prefix: string, count: number): ColourResult[] {
     const darkest = 0
     const lightest = 100
     const diff = (lightest - darkest) / count
@@ -33,13 +33,9 @@ export class ShadesGeneratorService {
       swatches.push(swatch)
     }
 
-    const padCount = Math.ceil((count / 100) * count) + 1
-
-    console.log('pad count', padCount)
-
     return swatches.map((swatch, idx) => {
       return {
-        name: `grey_${Math.floor((idx+1) * 100)}`,
+        name: `${prefix}_${Math.floor((idx+1) * 100)}`,
         androidCompose: this.androidColourCompose(swatch.string()),
         hex: swatch.hex(),
         hsl: swatch.hsl().string(),
@@ -48,50 +44,17 @@ export class ShadesGeneratorService {
     })
   }
 
-  androidColourCompose(colour: string): string {
+  private androidColourCompose(colour: string): string {
     const hex = Color(colour).hex().replace('#', '')
 
     return `Color(0xFF${hex})`
   }
 }
 
-type ColourResult = {
+export type ColourResult = {
   name: string
   androidCompose: string
-  hex: string, rgb: string
+  hex: string
+  rgb: string
   hsl: string
 }
-
-
-
-// var x = 0
-// var y = 100
-// var n = 18
-// var diff = (y-x)/n
-
-// var d = x
-// for (var i=x; i < n; i++) {
-//   d += diff;
-//   console.log(d);
-// }
-
-/*
-5.555555555555555
-11.11111111111111
-16.666666666666664
-22.22222222222222
-27.77777777777778
-33.333333333333336
-38.88888888888889
-44.44444444444445
-50.00000000000001
-55.555555555555564
-61.11111111111112
-66.66666666666667
-72.22222222222223
-77.77777777777779
-83.33333333333334
-88.8888888888889
-94.44444444444446
-100.00000000000001
-*/
--- src/app/shades-generator/shades-generator.component.ts
+++ src/app/shades-generator/shades-generator.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ColourResult, ShadesGeneratorService } from '../shades-generator.service';
+import { NgFor, NgStyle } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-shades-generator',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgStyle, FormsModule, ColorPickerModule],
   templateUrl: './shades-generator.component.html',
   styleUrl: './shades-generator.component.css'
 })
 export class ShadesGeneratorComponent {
+  constructor(
+    private shadesGeneratorService: ShadesGeneratorService,
+  ) {
+    this.swatches = this.shadesGeneratorService.shadesFromColour(
+      this.color,
+      this.colorName,
+      parseInt(this.count),
+    )
+  }
 
+  colorName: string = 'grey'
+  color: string = '#1d1d26'
+  count: string = '12'
+  swatches: ColourResult[] = []
+
+  setColorName(evt: Event) {
+    const newValue = (evt?.target as HTMLInputElement)?.value as string | null
+
+    if (newValue) {
+      this.colorName = newValue
+      this.recalculateShades()
+    }
+  }
+
+  setCount(evt: Event) {
+    const newValue = (evt?.target as HTMLInputElement)?.value as string | null
+
+    if (newValue) {
+      this.count = newValue
+      this.recalculateShades()
+    }
+  }
+
+  recalculateShades() {
+    const result = this.shadesGeneratorService.shadesFromColour(
+      this.color,
+      this.colorName,
+      parseInt(this.count),
+    )
+
+    this.swatches = result
+
+    // todo: generate code samples for android xml, android compose, css rgb,hsl,hex
+  }
 }
